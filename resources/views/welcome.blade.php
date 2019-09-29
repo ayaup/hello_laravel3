bfc42a92e1a685f6eb1aed991e68a5092bb611e0
kaodao <shkaodao@icloud.com>
2019-09-29 15:14:05 +0800

    Add .editorconfig

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel study
                 </div>
 
                 <div class="links">
