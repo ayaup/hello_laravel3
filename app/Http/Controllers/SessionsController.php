353ef80942a708f4b9f7de7c1eb54b99c73adfd8
Kaodao <shkaodao@icloud.com>
2019-10-11 21:25:44 -0500

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,25 +6,38 @@ use Illuminate\Http\Request;
 use Auth;
 
 class SessionsController extends Controller
+
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+    
+
     public function create()
     {
         return view('sessions.create');
     }
     public function store(Request $request)
     {
-       $credentials = $this->validate($request, [
-           'email' => 'required|email|max:255',
-           'password' => 'required'
-       ]);
-       if (Auth::attempt($credentials, $request->has('remember'))) {
-        session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
-    } else {
-        session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-           return redirect()->back()->withInput();
-    }
-       return;
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            $fallback = route('users.show', Auth::user());
+
+            return redirect()->intended($fallback);
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+        return;
     }
     public function destroy()
     {
@@ -32,5 +45,4 @@ class SessionsController extends Controller
         session()->flash('success', '您已成功退出！');
         return redirect('login');
     }
-
 }
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\User;
+
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function update(User $currentUser, User $user)
+    {
+        return $currentUser->id === $user->id;
+    }
+}
